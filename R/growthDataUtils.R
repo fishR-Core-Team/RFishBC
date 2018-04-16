7adf246729e208d2dbebd11800b2b79074172431
Derek Ogle <dogle@northland.edu>
2018-04-16 09:40:08 -0500

    Slight mods to documentation

--- R/growthDataUtils.R
+++ R/growthDataUtils.R
@@ -1,20 +1,20 @@
-#' @title Converts between types of measurements from calcified structures.
+#' @title Converts between types of measurements.
 #'
-#' @description Converts wide format growth data from radial to incremental or incremental to radial measurements.
+#' @description Converts one-fish-per-line (i.e., \dQuote{wide}) format growth data from radial to incremental or incremental to radial measurements.
 #'
-#' @details The data must be in WIDE format where each row contains all information (including all measurements from the calcified structure) for an individual fish.  It is assumed that the input data.frame is of the opposite data type given in \code{out.type} (i.e., that a conversion is needed).  It does not check to see if this is true.
-#' 
-#' The columns that contain the original measurement data can specified in a variety of ways.  First, if all columns begin with the same prefix (and no other columns contain that prefix), then the prefix string may be given to \code{in.pre=}.  Second, a sequence of column numbers may be given to \code{in.var=} with the \code{#:#} (if the columns are contiguous) or as a vector (if the columns are not contiguous).  Third, a vector of column names may be given to \code{in.var=}.  Note  that one, but not both, of \code{in.var=} or \code{in.pre=} must be specified by the user.
-#'
-#' The newly computed data will be labeled with a prefix the same as \code{out.type=} (i.e., \code{"rad"} or \code{"inc"}) unless \code{out.pre=} is set by the user.  For example, if the data are converted to radial measurements, then the output variables will be \dQuote{rad1}, \dQuote{rad2}, etc. unless \code{out.pre=} was changed from the default.  This function assumes that the measurements start with age-1.
-#'
-#' @param df A data.frame that contains the growth measurement data in wide or one-fish-per-line format.
-#' @param in.pre A string that indicates the prefix for all variable names in the input data frame that contain the measurements from the calcified structures.  See details.
-#' @param in.var A vector of column numbers or variable names in the input data frame that contain the measurements from the calcified structures.  See details.
-#' @param out.type A string that identifies the output format data type (i.e., the format to convert to).  If \code{"inc} (the default) the output data frame will be incremental measurements.  If \code{"rad} the output data frame will be radial measurements.
-#' @param out.pre A string that indicates the prefix to use for the newly computed measurements in the output data frame.  Defaults to the same string as \code{out.type}.
+#' @param df A data.frame that contains the growth measurement data in one-fish-per-line (i.e., \dQuote{wide}) format.
+#' @param in.pre A string that indicates the prefix for all variable names in the input data.frame that contain the measurements from the calcified structures. See details.
+#' @param in.var A vector of column numbers or variable names in the input data.frame that contain the measurements from the calcified structures. See details.
+#' @param out.type A string that identifies the output format data type (i.e., the format to convert to). If \code{"inc"} (the default) the output data frame will be incremental measurements. If \code{"rad"} the output data frame will be radial measurements.
+#' @param out.pre A string that indicates the prefix to use for the newly computed measurements in the output data frame. Defaults to the same string as \code{out.type}.
 #'
 #' @return A data.frame with all columns, except for those defined by \code{in.pre} or \code{in.var}, from the \code{df} retained as the left-most columns and the original data in the \code{in.var} columns converted to the \code{out.type} type as the remaining columns.
+#'
+#' @details The data must be in one-fish-per-line (i.e., \dQuote{wide}) format where each row contains all information (including all measurements from the calcified structure) for an individual fish. It is assumed that the input data.frame is of the opposite data type given in \code{out.type} (i.e., that a conversion is needed). It does not check to see if this is true.
+#' 
+#' The columns that contain the original measurement data can specified in a variety of ways. First, if all columns begin with the same prefix (and no other columns contain that prefix), then the prefix string may be given to \code{in.pre=}. Second, a sequence of column numbers may be given to \code{in.var=} with the \code{#:#} (if the columns are contiguous) or as a vector (if the columns are not contiguous). Third, a vector of column names may be given to \code{in.var=}. Note  that one, but not both, of \code{in.var=} or \code{in.pre=} must be specified by the user.
+#'
+#' The newly computed data will be labeled with a prefix the same as \code{out.type=} (i.e., \code{"rad"} or \code{"inc"}) unless \code{out.pre=} is set by the user. For example, if the data are converted to radial measurements, then the output variables will be \dQuote{rad1}, \dQuote{rad2}, etc. unless \code{out.pre=} was changed from the default. This function assumes that the measurements start with age-1.
 #'
 #' @author Derek H. Ogle, \email{derek@@derekogle.com}
 #'
@@ -75,20 +75,20 @@ gConvert<-function(df,in.pre=NULL,in.var=NULL,
 
 
 
-#' @title Add a total radius-at-capture variable to a data.frame that contains one-fish-per-line increments data.
+#' @title Add total radius-at-capture to wide increments data.
 #'
-#' @description Add a total radius-at-capture variable to a data.frame that contains one-fish-per-line increments data (i.e., \dQuote{wide} format).
+#' @description Add a total radius-at-capture variable to a data.frame that contains one-fish-per-line (i.e., \dQuote{wide} format) increments data.
 #'
-#' @details #' The columns that contain the original measurement data can specified in a variety of ways.  First, if all columns begin with the same prefix (and no other columns contain that prefix), then the prefix string may be given to \code{in.pre=}.  Second, a sequence of column numbers may be given to \code{in.var=} with the \code{#:#} (if the columns are contiguous) or as a vector (if the columns are not contiguous).  Third, a vector of column names may be given to \code{in.var=}.  Note  that one, but not both, of \code{in.var=} or \code{in.pre=} must be specified by the user.
+#' @details The columns that contain the original increment data can specified in a variety of ways. First, if all columns begin with the same prefix (and no other columns contain that prefix), then the prefix string may be given to \code{in.pre=}. Second, a sequence of column numbers may be given to \code{in.var=} with \code{#:#} (if the columns are contiguous) or as a vector (if the columns are not contiguous). Third, a vector of column names may be given to \code{in.var=}. Note  that one, but not both, of \code{in.var=} or \code{in.pre=} must be specified by the user.
 #'
-#' Note that the computed total radius-at-capture will only be the actual total radius-at-capture if all growth, including plus-growth in the current season, is recorded in the input data frame.
+#' Note that the computed total radius-at-capture will only be the actual total radius-at-capture if all growth, including \dQuote{plus-growth} in the current season, is recorded in the input data frame.
 #'
 #' @param df A data.frame that contains the growth increment data in one-fish-per-line (i.e., \dQuote{wide}) format.
-#' @param in.pre A string that represents the common part of the increment variable names.  See details.
-#' @param in.var A vector of variables in \code{df} that do not change.  See details.
-#' @param var.name A string that indicates the name for the new total radius-at-capture variable in the new data.frame.
+#' @param in.pre A string that represents the common part of the increment variable names. See details.
+#' @param in.var A vector of variables in \code{df} that do not change. See details.
+#' @param var.name A string that indicates the name for the new total radius-at-capture variable in the new data.frame. Defaults to \dQuote{radcap}.
 #'
-#' @return A data.frame of increments in one-fish-per-line  (i.e., \dQuote{wide}) format with the total radius-at-capture appended in a variable named as given in \code{var.name} (defaults to \dQuote{radcap}).
+#' @return A data.frame of increments in one-fish-per-line  (i.e., \dQuote{wide}) format with the total radius-at-capture appended in a variable named as given in \code{var.name}.
 #'
 #' @seealso See \code{\link{gConvert}} for related functionality.
 #'
@@ -127,21 +127,27 @@ addRadCap <- function(df,in.pre=NULL,in.var=NULL,var.name="radcap") {
 ##############################################################
 bcUtilChecker <- function(df,in.pre,in.var) {
   ## Some checks
-  if (is.null(in.pre) & is.null(in.var)) stop("You must use one of 'in.pre=' or 'in.var='.",call.=FALSE)
-  if (!is.null(in.pre) & !is.null(in.var)) warning("Both 'in.var=' and 'in.pre=' were given; only 'in.var=' will be used.",call.=FALSE)
+  if (is.null(in.pre) & is.null(in.var)) stop("You must use one of 'in.pre=' or 'in.var='.",
+                                              call.=FALSE)
+  if (!is.null(in.pre) & !is.null(in.var)) warning("Both 'in.var=' and 'in.pre=' were given; only 'in.var=' will be used.",
+                                                   call.=FALSE)
   ## Find variables with measurements
   if (!is.null(in.var)) {
     if (is.character(in.var)) {
-      if (!all(in.var %in% names(df))) stop("Not all 'in.var=' variables found.",call.=FALSE)
+      if (!all(in.var %in% names(df))) stop("Not all 'in.var=' variables found.",
+                                            call.=FALSE)
       # convert in.var names to column numbers
       in.var <- which(names(df) %in% in.var)
     } else {
-      if (max(in.var)>ncol(df)) stop("Column numbers exceed number of columns.",call.=FALSE)
-      if (min(in.var)<1) stop("Non-positive column number given.",call.=FALSE)
+      if (max(in.var)>ncol(df)) stop("Column numbers exceed number of columns.",
+                                     call.=FALSE)
+      if (min(in.var)<1) stop("Non-positive column number given.",
+                              call.=FALSE)
     }
   } else {
     in.var <- grep(paste0("^",in.pre),names(df)) 
-    if (length(in.var)==0) stop("No variables start with 'in.pre' string.",call.=FALSE)
+    if (length(in.var)==0) stop("No variables start with 'in.pre' string.",
+                                call.=FALSE)
   }
   ## Return in.var
   in.var
